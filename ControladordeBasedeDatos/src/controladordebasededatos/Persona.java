f2b4b008ad56d5d82214c9439e69e65afa8669c5
Fernando Ortega <33491534+jfernando95@users.noreply.github.com>
2018-03-18 22:41:22 -0500

    josue.ortega@unl.edu.ec

---
+++ ControladordeBasedeDatos/src/controladordebasededatos/Persona.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladordebasededatos;
+
+/**
+ *
+ * @author Ferchito
+ */
+public class Persona {
+    int id;
+    String nombres;
+    String apellidos;
+    String email;
+    String telefono;
+
+    public Persona(int id, String nonmbres, String apellido, String email, String telefono) {
+        this.id = id;
+        this.nombres = nonmbres;
+        this.apellidos = apellido;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+ 
+    
+}
